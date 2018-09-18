01a04b4cde5cfb355e5b5ff3faa1157adecc683a
Huy Tran <tndhuy@tma.com.vn>
2018-09-18 18:12:49 +0700

    render messages content

--- src/reducers/activeUserId.js
+++ src/reducers/activeUserId.js
@@ -1,10 +1,10 @@
 import { SET_ACTIVE_USER_ID } from "../constants/action-type";
 
-export default function activeUserId(state = null, action) {
+export default (state = null, action) => {
   switch (action.type) {
     case SET_ACTIVE_USER_ID:
       return action.payload;
     default:
       return state;
   }
-}
+};
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,5 @@
 import { generateUser } from "../static-data";
 
-export default function user(state = generateUser(), action) {
+export default (state = generateUser(), action) => {
   return state;
-}
+};
